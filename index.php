508386a74855eaca131946247a29e5f14fd77591
devprogremer <devprogremer@gmail.com>
2023-01-27 19:32:54 +0700

    rev

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <img src="assets/img/okkkkkk.png" alt="">
         <h1>SMK BINA CENDEKIA CIREBON</h1>
